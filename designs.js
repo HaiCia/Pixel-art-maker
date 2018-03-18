14f3bcee759e1061fe14ac417c9140d19764fe4b
katarzyna <kasiekk124@gmail.com>
2018-03-18 16:27:46 +0000

    done

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+
+
 // Select size input
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWeight');
+
+// Select table;
+const table = document.getElementById('pixelCanvas');
+
 
 // When size is submitted by the user, call makeGrid()
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function (e) {
+   e.preventDefault();
+   makeGrid();
+});
+
+// Color changing 
+table.addEventListener('mousedown', function (e) {
+   e.target.style.backgroundColor = colorPicker.value;
+})
 
 function makeGrid() {
 
-// Your code goes here!
+   table.textContent = "";
 
+   for (let i = 1; i <= gridHeight.value; i++) {
+      const trow = document.createElement('tr');
+      //trow.textContent = `row`;
+      table.appendChild(trow);
+
+      for (let j = 1; j <= gridWidth.value; j++) {
+         const tcol = document.createElement('td');
+         //tcol.textContent = `col`;
+         trow.appendChild(tcol);
+      }
+   }
 }
